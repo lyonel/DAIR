b824c74d4b24471b33dd9e21fed7d8a746020488
Lyonel Vincent <lyonel@ezix.org>
2012-08-10 11:29:02 +0200

    keep track of close/reopen actions

--- dair.sql
+++ dair.sql
@@ -3,9 +3,11 @@ CREATE TABLE entries(title TEXT NOT NULL, category TEXT COLLATE NOCASE, summary 
 CREATE TABLE keywords(keyword TEXT NOT NULL COLLATE NOCASE, entryid INTEGER NOT NULL);
 CREATE TABLE notes(summary TEXT NOT NULL, entryid INTEGER NOT NULL, timestamp DATE DEFAULT CURRENT_TIMESTAMP, author TEXT COLLATE NOCASE);
 CREATE VIEW persons AS SELECT owner AS person,ROWID AS entryid FROM entries UNION SELECT author AS person,entryid FROM notes;
-CREATE VIEW tags AS SELECT keyword AS tag,entryid FROM keywords UNION SELECT category AS tag,ROWID AS entryid FROM entries UNION SELECT project AS tag,ROWID AS entryid FROM entries UNION SELECT 'overdue' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)<=DATE('now','localtime')) UNION SELECT 'due this month' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)>=DATE('now', 'localtime', 'start of month')) AND (DATE(deadline)<DATE('now', 'localtime', 'start of month', '+1 month')) UNION SELECT 'due this week' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)>=DATE('now', 'localtime', 'weekday 0', '-7 day')) AND (DATE(deadline)<DATE('now', 'localtime', 'weekday 0')) UNION SELECT 'due next week' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)>=DATE('now', 'localtime', 'weekday 0')) AND (DATE(deadline)<DATE('now', 'localtime', 'weekday 0', '+7 day'));
-CREATE TRIGGER close AFTER UPDATE OF status ON entries WHEN new.status IN ('closed', 'solved') BEGIN
+CREATE VIEW tags AS SELECT keyword AS tag,entryid FROM keywords UNION SELECT category AS tag,ROWID AS entryid FROM entries UNION SELECT project AS tag,ROWID AS entryid FROM entries UNION SELECT 'overdue' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)<=DATE('now','localtime')) UNION SELECT 'due this month' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)>=DATE('now', 'localtime', 'start of month')) AND (DATE(deadline)<DATE('now', 'localtime', 'start of month', '+1 month')) UNION SELECT 'due this week' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)>=DATE('now', 'localtime', 'weekday 0', '-7 day')) AND (DATE(deadline)<DATE('now', 'localtime', 'weekday 0')) UNION SELECT 'due next week' AS tag,ROWID AS entryid FROM entries WHERE open AND (DATE(deadline)>=DATE('now', 'localtime', 'weekday 0')) AND (DATE(deadline)<DATE('now', 'localtime', 'weekday 0', '+7 day')) UNION SELECT 'no owner'AS tag,ROWID AS entryid FROM entries WHERE open AND owner IS NULL;
+CREATE TRIGGER close AFTER UPDATE OF status ON entries WHEN new.status IN ('closed', 'solved')
+BEGIN
 UPDATE entries SET open=0 WHERE ROWID=old.ROWID;
+INSERT INTO notes (summary, entryid) VALUES ('closed', old.ROWID);
 END;
 CREATE TRIGGER entry_delete AFTER DELETE ON entries
 BEGIN
@@ -22,6 +24,8 @@ END;
 CREATE TRIGGER note_update AFTER INSERT ON notes BEGIN
 UPDATE entries SET updated=DATETIME('now') WHERE ROWID=new.entryid;
 END;
-CREATE TRIGGER reopen AFTER UPDATE OF status ON entries WHEN new.status NOT IN ('closed', 'solved') BEGIN
+CREATE TRIGGER reopen AFTER UPDATE OF status ON entries WHEN new.status NOT IN ('closed', 'solved')
+BEGIN
 UPDATE entries SET open=1 WHERE ROWID=old.ROWID;
+INSERT INTO notes (summary, entryid) VALUES ('reopened', old.ROWID);
 END;
